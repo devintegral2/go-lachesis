ed82270d8ebac6f5aba5fb43d8f9cf113b13a82d
IntegralTeam V <repo@sfxdx.ru>
2020-06-23 12:51:36 +0700

    report unlimit loop in test

--- run-lachesis.sh
+++ run-lachesis.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+FAKENET_DIR=~/.lachesis/fakenet-1
+FAKENET_KEYSTOR=$FAKENET_DIR/keystore
+MAINNET_KEYSTOR=/home/user/.lachesis/keystore
+if [ ! -d "$FAKENET_DIR" ]; then
+  ./build/lachesis --fakenet 1/1 --rpc --rpcapi="eth,debug,admin,web3,personal,net,txpool,ftm,sfc" --ws --wsorigins="*" --wsapi="eth,debug,admin,web3,personal,net,txpool,ftm,sfc" --config=config.toml
+fi
+
+NUMBER_KEYS=$(ls -1 "$FAKENET_KEYSTOR" | wc -l)
+if [[ $NUMBER_KEYS < 2 ]]; then
+  ./build/lachesis account new
+  ./build/lachesis account new
+  cp $MAINNET_KEYSTOR/* $FAKENET_KEYSTOR
+  echo "added key to $FAKENET_KEYSTOR"
+fi
 ./build/lachesis --fakenet 1/1 --rpc --rpcapi="eth,debug,admin,web3,personal,net,txpool,ftm,sfc" --ws --wsorigins="*" --wsapi="eth,debug,admin,web3,personal,net,txpool,ftm,sfc" --config=config.toml
